97031fd79983291e3a83752ac050bd85f3d85965
Daichi Harada <haradadaichi715@gmail.com>
2015-01-03 08:23:57 +0900

    almost done

--- Gemfile
+++ Gemfile
@@ -1,1 +1,7 @@
 source 'https://rubygems.org'
+
+ruby '2.1.4'
+gem 'sinatra'
+gem 'nokogiri'
+gem 'zipruby'
+gem 'eventmachine'
--- app.rb
+++ app.rb
@@ -6,25 +6,58 @@ require 'uri'
 require 'zipruby'
 require 'find'
 require 'nkf'
+require 'eventmachine'
 
 get '/' do
   erb :index
 end
 
 get '/:id' do
-  make_zip(params[:id])
-  send_file("imgs/#{params[:id]}.zip")
-  redirect "/"
+  if FileTest.exist?("./tmp/imgs/#{params[:id]}.zip")
+    @flag_getting = false
+  else
+    @flag_getting = true
+  end
+  erb :id
+end
+
+get '/download/:id' do
+  send_file("./tmp/imgs/#{params[:id]}.zip")
 end
 
 post '/give_me_img' do
-  id = params[:url].gsub("http://","").gsub("matome.naver.jp/odai/","")
-  get_imgs(id)
+  id = params[:url].gsub("http://","").gsub("matome.naver.jp/odai/","").gsub("matome.naver.jp/m/odai/","").gsub("/","")
+
+  if id == nil
+    redirect "/"
+  end
+
+  EM::defer do
+    get_imgs(id)
+    make_zip(id)
+  end
   redirect "/#{id}"
 end
 
 helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+
+  def id_is_valid?(id)
+    #TODO
+    #数字だけ？
+    #page = Nokogiri::HTML(open("http://matome.naver.jp/odai/#{id}"))
+    #開ける？
+    true
+  end
+
+  def file_present?(id)
+    #TODO
+  end
+
   def get_imgs(id)
+    dirName = "./tmp/imgs/#{id}/"
+    return if FileTest.exist?(dirName)
     page = Nokogiri::HTML(open("http://matome.naver.jp/odai/#{id}"))
     first_url = page.search('div.mdMTMWidget01ItemImg01')[0].search('a').attribute('href').value
     page = Nokogiri::HTML(open(first_url))
@@ -34,7 +67,6 @@ helpers do
       p next_url = page.search('p.mdMTMEnd01Pagination01').search('a.mdMTMEnd01Pagination01Next').attribute('href').value
       page = Nokogiri::HTML(open(next_url))
       p fileName = File.basename(img_url)
-      dirName = "./imgs/#{id}/"
       filePath = dirName + fileName
       FileUtils.mkdir_p(dirName) unless FileTest.exist?(dirName)
       open(filePath, 'wb') do |output|
@@ -43,12 +75,15 @@ helpers do
         end
       end
       i += 1
+      #とりあえず100枚でブレイク
+      break if i == 100
     }
   rescue
   end
+
   def make_zip(id)
-    Zip::Archive.open("imgs/#{id}.zip", Zip::CREATE) do |ar|
-      Find.find("imgs/#{id}") do |path|
+    Zip::Archive.open("./tmp/imgs/#{id}.zip", Zip::CREATE) do |ar|
+      Find.find("./tmp/imgs/#{id}") do |path|
         entry = NKF.nkf('-U -s -Lw', path)
         if File.directory?(path)
           ar.add_dir(entry)
@@ -57,5 +92,7 @@ helpers do
         end
       end
     end
+  rescue
   end
+
 end
